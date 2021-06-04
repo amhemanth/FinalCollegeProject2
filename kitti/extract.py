6e44091f1f00e924b7e4bebc003629e689477e75
AM_Hemanth <40871106+amhemanth@users.noreply.github.com>
2021-06-04 12:14:08 +0530

    Add files via upload

---
+++ kitti/extract.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+extract ground truth box and distance for each object
+"""
+import os
+import string
+import numpy as np
+
+#fo = open("train_guo_final.txt","w")
+fo = open("train_guo_final.txt","w")   ## label.txt
+dict = {'Car':1, 'Person':0}
+#path="/media/smart/05F013960677C1A7/Jichen_Thesis/code/kitti/training/label/"  # reading path
+path="training/label_2/" 
+path_list=os.listdir(path)
+path_list.sort() 
+i_0,i_1,i_2,i_3 = 0,0,0,0
+for file_name in path_list:
+    full_path = os.path.join(path, file_name)
+    name = file_name[:-4]  # extract filename before.txt
+    img_name = name + '.png'
+    img_path = os.path.join(os.getcwd(), "training/image_2/"+img_name)
+    #print(os.path.abspath(img_name)) 
+    #print(img_path)
+    fo.write(img_path)
+    fo.write(' ')
+    txt = open(full_path)
+    split_lines = txt.readlines()
+    for split_line in split_lines:
+        line=split_line.strip().split()
+
+        fo.write(str(int(float(line[4]))) + ',')
+        fo.write(str(int(float(line[5]))) + ',')
+        fo.write(str(int(float(line[6]))) + ',')
+        fo.write(str(int(float(line[7]))) + ',')
+        fo.write(str(dict[line[0].title()]) + ',')
+        fo.write(str(float(line[-2]))+ ' ')
+        #fo.write(str(float(np.sqrt(np.square(float(line[-2]))+np.square(float(line[-4]))))) + ' ')
+
+    fo.write('\n')
+fo.close()
+
+
